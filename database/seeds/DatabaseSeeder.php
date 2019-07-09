f8ffbee4f1d4a71ac71ef74fe4009e311fc969a8
Facundo Fierro <bluelachrymologist@gmail.com>
2019-07-09 16:28:19 -0300

    added seeder call

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            CategoriesTableSeeder::class,
+            SubCategoriesTableSeeder::class,
+            UsersTableSeeder::class,
+            ProductsTableSeeder::class,
+            MultimediaTableSeeder::class
+        ]);
     }
 }
